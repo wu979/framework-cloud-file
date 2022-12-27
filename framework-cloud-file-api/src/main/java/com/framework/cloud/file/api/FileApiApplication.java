7360938a8565ccf3324c82e00b791514e1cce51f
吴思伟 <18683789594@163.com>
2022-12-27 15:14:54 +0800

    first commit

---
+++ framework-cloud-file-api/src/main/java/com/framework/cloud/file/api/FileApiApplication.java
@@ -0,0 +1,13 @@
+package com.framework.cloud.file.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileApiApplication.class, args);
+    }
+
+}
