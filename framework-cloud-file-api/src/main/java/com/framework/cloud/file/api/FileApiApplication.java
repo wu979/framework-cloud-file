b89b8bfa61884c89870db235f4ebcd63145e1292
wusiwei <18683789594@163.com>
2022-12-27 16:40:42 +0800

    commit

--- framework-cloud-file-api/src/main/java/com/framework/cloud/file/api/FileApiApplication.java
+++ framework-cloud-file-api/src/main/java/com/framework/cloud/file/api/FileApiApplication.java
@@ -1,13 +1,20 @@
 package com.framework.cloud.file.api;
 
+import com.framework.cloud.core.annotation.FrameworkApplication;
+import com.framework.cloud.feign.annotation.EnableFeignInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@Slf4j
+@EnableFeignInterceptor
+@EnableDiscoveryClient
+@FrameworkApplication
 public class FileApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FileApiApplication.class, args);
+        log.info("File service started successfully");
     }
 
 }
